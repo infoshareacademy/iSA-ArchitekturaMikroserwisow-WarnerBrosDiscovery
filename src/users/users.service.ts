29fb51edd921f8c95416350003c20f97e9be64fa
Dominik Młynarczyk <dominik.mlynarczyk@atarise.com>
2024-03-18 11:47:55 +0100

    feat: Use Nest.js built-in exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const users = [
+  {
+    id: 1,
+    name: 'John',
+  },
+  {
+    id: 2,
+    name: 'Doe',
+  },
+];
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
