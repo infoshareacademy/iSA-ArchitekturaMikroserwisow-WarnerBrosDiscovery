ce71c355f9f85555ab9509a5200cb3ee1594210b
Dominik Młynarczyk <dominik.mlynarczyk@atarise.com>
2024-03-19 12:16:01 +0100

    refactor: handle DBException when user is not found during verification

--- apps/auth/src/stategies/local.strategy.ts
+++ apps/auth/src/stategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
@@ -10,10 +10,11 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string) {
-    const user = await this.userService.verifyUser(email, password);
-    if (!user) {
-      throw new Error('User not found');
+    try {
+      const user = await this.userService.verifyUser(email, password);
+      return user;
+    } catch (e) {
+      throw new UnauthorizedException('Unathorized');
     }
-    return user;
   }
 }
